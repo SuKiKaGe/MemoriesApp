80b2d3618f9e3c9f76ce3f58ed649d2ac0beb46e
SuKiKaGe <SuKiKaGe@gmail.com>
2019-11-28 12:16:49 +0100

    Initial Commit

---
+++ app/src/main/java/es/usj/mastertsa/fm/memoriesapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package es.usj.mastertsa.fm.memoriesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
