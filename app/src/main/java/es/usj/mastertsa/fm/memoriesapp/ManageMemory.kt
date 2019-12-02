ab0253311d68a3f1fe1d38453f9251d29d36ad9d
SuKiKaGe <SuKiKaGe@gmail.com>
2019-12-02 13:09:01 +0100

    - ManageMemory - Included Photo Capture Functionality (Needs Testing) - ManageMemory - Included Video Capture Functionality (Needs Testing) - ManageMemory - Included Audio Capture Functionality (Needs Testing) - activity_create_memory - Layout Modified According to Requisites - AndroidManifest - Permissions Request Required for Camera / Audio

--- app/src/main/java/es/usj/mastertsa/fm/memoriesapp/ManageMemory.kt
+++ app/src/main/java/es/usj/mastertsa/fm/memoriesapp/ManageMemory.kt
@@ -1,20 +1,29 @@
 package es.usj.mastertsa.fm.memoriesapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_create_memory.*
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 const val ACTION = "New/Edit Memory"
 const val ID = "Memory Id"
+const val REQUEST_IMAGE_CAPTURE = 1
+const val REQUEST_VIDEO_CAPTURE = 2
+const val REQUEST_AUDIO_CAPTURE = 3
 
 class ManageMemory : AppCompatActivity()
 {
     var idToEdit: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -37,9 +46,33 @@ class ManageMemory : AppCompatActivity()
                 Toast.makeText(this, "Unknown Action For Memory", Toast.LENGTH_SHORT).show()
             }
         }
+
+        btnPhoto.setOnClickListener {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
+                    takePictureIntent -> takePictureIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+
+        btnVideo.setOnClickListener {
+            Intent(MediaStore.ACTION_VIDEO_CAPTURE).also {
+                    takeVideoIntent -> takeVideoIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE)
+                }
+            }
+        }
+
+        btnAudio.setOnClickListener {
+            Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION).also {
+                    takeAudioIntent -> takeAudioIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takeAudioIntent, REQUEST_AUDIO_CAPTURE)
+                }
+            }
+        }
     }
 
-    fun setEditData(id: Int)
+    private fun setEditData(id: Int)
     {
         val memory = MemoriesManager.instance.memories.find { it.id == id }
 
@@ -59,10 +92,8 @@ class ManageMemory : AppCompatActivity()
         when(item!!.itemId)
         {
             R.id.save -> {
-                val newMemory = Memory("Recuerdo 1", "Descripción de recuerdo 1")
+                val newMemory = Memory(etTitle.text.toString(), etDescription.text.toString())
                 newMemory.id = idToEdit
-                newMemory.title = etTitle.text.toString()
-                newMemory.description = etDescription.text.toString()
 
                 when (title)
                 {
@@ -88,5 +119,27 @@ class ManageMemory : AppCompatActivity()
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
+    {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
+        {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imgBtnPhoto.setImageBitmap(imageBitmap)
+        }
+        else if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK)
+        {
+            val videoUri: Uri = intent.data!!
+            vvBtnVideo.setVideoURI(videoUri)
+            //val imageBitmap = data?.extras?.get("data") as Bitmap
+            //imgBtnVideo.setImageBitmap(imageBitmap)
+        }
+        else if (requestCode == REQUEST_AUDIO_CAPTURE && resultCode == RESULT_OK)
+        {
+                val audioUri : Uri = intent.data!!
+                // make use of this MediaStore uri
+                // e.g. store it somewhere
+        }
     }
 }
